522e5877b5f9d985efb538e3a37650f0867f9523
Luke W Johnston <lwjohnst@gmail.com>
2016-11-23 22:54:08 -0500

    Minor edit to extdata path option

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
     op.PROMISE <- list(
         PROMISE.rename.path = tempdir(),
         PROMISE.data.raw.path = tempdir(),
+        PROMISE.extdata.path = tempdir(),
         PROMISE.visit.numbers = 0:3
     )
     toset <- !(names(op.PROMISE) %in% names(op))
